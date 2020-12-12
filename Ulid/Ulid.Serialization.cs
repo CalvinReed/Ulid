83f5ef7c7351d97807ae9f4676500f5e7494638e
Calvin Reed <calvinreed@posteo.net>
2020-12-12 13:36:37 -0500

    Parsing methods: - Added "string?" overloads - Added whitespace trimming to match Guid parsing - Improved documentation and variable names

--- Ulid/Ulid.Serialization.cs
+++ Ulid/Ulid.Serialization.cs
@@ -14,43 +14,6 @@ namespace CalvinReed
             return new string(digits);
         }
 
-        /// <summary>
-        /// Parses the input as a <see cref="Ulid"/>.
-        /// </summary>
-        /// <param name="str">
-        /// The span of characters to be parsed.
-        /// </param>
-        /// <returns>
-        /// The parsed <see cref="Ulid"/>.
-        /// </returns>
-        /// <exception cref="FormatException">
-        /// The input is not valid.
-        /// </exception>
-        public static Ulid Parse(ReadOnlySpan<char> str)
-        {
-            return TryParse(str) ?? throw new FormatException($"Input is not a valid {nameof(Ulid)}");
-        }
-
-        /// <summary>
-        /// Attempts to parse the input as a <see cref="Ulid"/>.
-        /// </summary>
-        /// <param name="str">
-        /// The characters to be parsed.
-        /// </param>
-        /// <returns>
-        /// If input is valid, the parsed <see cref="Ulid"/>.
-        /// Otherwise, <c>null</c>.
-        /// </returns>
-        public static Ulid? TryParse(ReadOnlySpan<char> str)
-        {
-            Span<byte> data = stackalloc byte[BinarySize + 1];
-            var success =
-                str.Length == Base32Length      // Check length,
-                && Base32.TryDecode(str, data)  // then check chars,
-                && data[0] == 0;                // then check for overflow.
-            return success ? new Ulid(data[1..]) : (Ulid?) null;
-        }
-
         internal void WriteDigits(Utf8JsonWriter writer)
         {
             Span<char> digits = stackalloc char[Base32Length];
@@ -64,6 +27,56 @@ namespace CalvinReed
             Misc.WriteULong(n0, data);
             Misc.WriteULong(n1, data[sizeof(ulong)..]);
             Base32.Encode(data, digits);
+        }
+
+        /// <inheritdoc cref="Parse(ReadOnlySpan{char})"/>
+        public static Ulid Parse(string? input)
+        {
+            return Parse(input.AsSpan());
+        }
+
+        /// <inheritdoc cref="TryParse(ReadOnlySpan{char})"/>
+        public static Ulid? TryParse(string? input)
+        {
+            return TryParse(input.AsSpan());
+        }
+
+        /// <summary>
+        /// Parses <see cref="input"/> as a <see cref="Ulid"/>.
+        /// </summary>
+        /// <returns>
+        /// The successfully parsed <see cref="Ulid"/>.
+        /// </returns>
+        /// <exception cref="FormatException">
+        /// <see cref="input"/> is not a valid <see cref="Ulid"/>.
+        /// </exception>
+        /// <inheritdoc cref="TryParse(ReadOnlySpan{char})"/>
+        public static Ulid Parse(ReadOnlySpan<char> input)
+        {
+            return TryParse(input) ?? throw new FormatException($"Input is not a valid {nameof(Ulid)}");
+        }
+
+        /// <summary>
+        /// Attempts to parse <see cref="input"/> as a <see cref="Ulid"/>.
+        /// </summary>
+        /// <remarks>
+        /// Trims leading and trailing whitespace.
+        /// </remarks>
+        /// <param name="input">
+        /// The characters to be parsed.
+        /// </param>
+        /// <returns>
+        /// If <see cref="input"/> is valid, the parsed <see cref="Ulid"/>. Otherwise, <c>null</c>.
+        /// </returns>
+        public static Ulid? TryParse(ReadOnlySpan<char> input)
+        {
+            Span<byte> data = stackalloc byte[BinarySize + 1];
+            var trimmed = input.Trim();
+            var success =
+                trimmed.Length == Base32Length     // Check length,
+                && Base32.TryDecode(trimmed, data) // then check chars,
+                && data[0] == 0;                   // then check for overflow.
+            return success ? new Ulid(data[1..]) : (Ulid?) null;
         }
     }
 }
